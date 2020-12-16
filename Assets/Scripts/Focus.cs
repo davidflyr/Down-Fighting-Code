eff6ec81340886b86f528bbe7eb0da3379d81710
MeanBachine <david.flyr@gmail.com>
2020-12-15 19:38:06 -0600

    Add Focus script
    Changed the vcam focus group to focus on "focus" points that are children of the character GameObjects, then wrote a script that adjusts their y positions based on how far apart the characters are.

---
+++ Assets/Scripts/Focus.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Focus : MonoBehaviour
+{
+    [SerializeField] Focus _opponent;
+    [SerializeField] float _maxDistance = 7.36f;
+    [SerializeField] float _maxYOffset = 1.5f;
+    [SerializeField] float _baseYOffset = 2.0f;
+
+    Transform parent;
+    float _distance;
+    float _yOffset;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponent<Transform>().parent;
+        _distance = Mathf.Abs(transform.position.x - _opponent.transform.position.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distance = Mathf.Abs(transform.position.x - _opponent.transform.position.x);
+        _yOffset = (_distance > 0.5f) ? _maxYOffset/_maxDistance * _distance + _baseYOffset : _baseYOffset;
+
+        transform.position = new Vector3(
+            parent.position.x,
+            parent.position.y + _yOffset,
+            parent.position.z
+            );
+    }
+}
